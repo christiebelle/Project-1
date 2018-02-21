8a70826fc035d338bd42a2919350509a68fba4ae
Christiebelle <cslester@outlook.com>
2018-02-21 10:02:35 +0000

    minor edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,38 +14,38 @@ pet1 = Animal.new({
   "photo" => "http://cdn3-www.cattime.com/assets/uploads/2011/12/file_2674_himalayan-460x290-460x290.jpg"
   })
 
-pet1.save()
+  pet1.save()
 
-pet2 = Animal.new({
-  "name" => "Henry",
-  "type" => "Dog",
-  "breed" => "Japanese Akita",
-  "adoptable" => "No - Training Needed",
-  "admission" => "10/28/2017",
-  "photo" => "https://www.hyperassur.com/wp-content/uploads/2017/07/Akita-Inu.jpg"
-  })
+  pet2 = Animal.new({
+    "name" => "Henry",
+    "type" => "Dog",
+    "breed" => "Japanese Akita",
+    "adoptable" => "No - Training Needed",
+    "admission" => "10/28/2017",
+    "photo" => "https://www.hyperassur.com/wp-content/uploads/2017/07/Akita-Inu.jpg"
+    })
 
-pet2.save()
+    pet2.save()
 
-pet3 = Animal.new({
-  "name" => "Elizabeth",
-  "type" => "Cat",
-  "breed" => "Bengal Cat",
-  "adoptable" => "Yes",
-  "admission" => "08/08/2017",
-  "photo" => "https://www.pets4homes.co.uk/images/breeds/31/7711e1514d7d6665a240134a57ee13ad.jpg"
-  })
+    pet3 = Animal.new({
+      "name" => "Elizabeth",
+      "type" => "Cat",
+      "breed" => "Bengal Cat",
+      "adoptable" => "Yes",
+      "admission" => "08/08/2017",
+      "photo" => "https://www.pets4homes.co.uk/images/breeds/31/7711e1514d7d6665a240134a57ee13ad.jpg"
+      })
 
-pet3.save()
+      pet3.save()
 
-pet4 = Animal.new({
-  "name" => "Victoria",
-  "type" => "Dog",
-  "breed" => "Shiba Inu",
-  "adoptable" => "Yes",
-  "admission" => "12/02/2017",
-  "photo" => "https://i.imgur.com/2qEk2WZ.jpg"
-  })
+      pet4 = Animal.new({
+        "name" => "Victoria",
+        "type" => "Dog",
+        "breed" => "Shiba Inu",
+        "adoptable" => "Yes",
+        "admission" => "12/02/2017",
+        "photo" => "https://i.imgur.com/2qEk2WZ.jpg"
+        })
 
 pet4.save()
 
